892531e4ce3dac9976384bcec547de51f84114f6
typsoon <polarny.p@onet.pl>
2025-04-04 17:47:02 +0200

    Three hypotheses

--- regression.sync.py
+++ regression.sync.py
@@ -17,6 +17,7 @@
 from abc import abstractmethod
 import numpy as np
 import pandas as pd
+from scipy.sparse import data
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
@@ -37,6 +38,7 @@ class BaseFunc:
         return self._parameters
 
     def update_parameters(self, new_parameters):
+        # print("New par shape: ", new_parameters.shape)
         self._parameters = new_parameters
 
 
@@ -68,15 +70,25 @@ class Model:
     def train(self, epochs_num, data, labels, loss_at_epoch=None):
         for i in range(epochs_num):
             prediction = self.base_func.predict(data)
+            # print(prediction.shape)
 
-            reg_derivative = self.regularization_derivative(base_func.parameters())
+            reg_derivative = self.regularization_derivative(self.base_func.parameters())
+
+            assert (
+                reg_derivative.shape == self.base_func.parameters().shape
+            ), f"""This should not happen: reg_derivative.shape: {reg_derivative.shape} != 
+                parameters_shape: {self.base_func.parameters().shape}"""
+
             # print(reg_derivative.shape)
             gradient = (
                 self.base_func.derivative(data).T
                 @ self.loss_func.loss_derivative(prediction, labels)
                 + reg_derivative
             )
-            # print(gradient.shape)
+            # print(
+            #     f"bsd: {self.base_func.derivative(data).T.shape}, lsd: {self.loss_func.loss_derivative(prediction, labels).shape}, reg_der: {reg_derivative.shape}"
+            # )
+            # print("Gradient shape: ", gradient.shape)
 
             new_parameters = self.base_func.parameters() - self.learning_rate * gradient
 
@@ -89,8 +101,13 @@ class Model:
             #     self.learning_rate *= 0.99
 
     def test(self, data, labels):
+        # print(data.shape)
         prediction = self.base_func.predict(data)
-        return self.loss_func.loss_value(prediction, labels)
+        answer = self.loss_func.loss_value(prediction, labels)
+        # print(np.array(answer).shape)
+        assert np.array(answer).shape == (), f"Found shape: {np.array(answer).shape}"
+
+        return answer
 
 
 # %% [markdown]
@@ -100,11 +117,13 @@ class Model:
 # %%
 class OrdinaryBaseFunc(BaseFunc):
     def __init__(self, degree, number_of_inputs) -> None:
+        # print(degree)
         self._parameters = np.random.randn(degree)
 
     def predict(self, data):
         # temp = data @ self._parameters
-        # print(temp.shape)
+        # print(self._parameters.shape)
+
         return data @ self._parameters
 
     def derivative(self, data):
@@ -158,9 +177,10 @@ def divide_data(data, test_ratio):
 
 
 train_set, test_set = divide_data(df, test_ratio=0.2)
-train_set, validation_set = divide_data(train_set, test_ratio=0.2)
+train_set, validation_set = divide_data(train_set, test_ratio=0.2 / 0.6)
 # %%
 train_labels = train_set.pop(train_set.columns[7])
+validation_labels = validation_set.pop(validation_set.columns[7])
 test_labels = test_set.pop(test_set.columns[7])
 # train_set.insert(0, "Ones", 1)
 train_set.head()
@@ -242,6 +262,9 @@ print(train_labels_matrix.shape)
 
 # %%
 
+validation_matrix, validation_labels_matrix = to_matrices(
+    validation_set, validation_labels, preprocess_data
+)
 test_matrix, test_labels_matrix = to_matrices(test_set, test_labels, preprocess_data)
 
 print(test_matrix.shape, test_labels_matrix.shape)
@@ -254,10 +277,15 @@ print(test_matrix.shape, test_labels_matrix.shape)
 
 # %%
 def train_and_test(
-    model, train_matrix, train_labels_matrix, test_matrix, test_labels_matrix
+    model,
+    train_matrix,
+    train_labels_matrix,
+    test_matrix,
+    test_labels_matrix,
+    epochs_num=700,
 ):
     loss_at_epochs = [model.test(train_matrix, train_labels_matrix)]
-    model.train(700, train_matrix, train_labels_matrix, loss_at_epochs)
+    model.train(epochs_num, train_matrix, train_labels_matrix, loss_at_epochs)
     print(model.test(train_matrix, train_labels_matrix))
     print(model.test(test_matrix, test_labels_matrix))
 
@@ -311,4 +339,99 @@ model = Model(base_func_reg_L1, loss_func_reg_L1, 0.01, L1_regularization)
 # %%
 train_and_test(
     model, train_matrix, train_labels_matrix, test_matrix, test_labels_matrix
+)
+
+# %% [markdown]
+# # First Hypothesis
+
+
+# %%
+def test_hypothesis(
+    preprocess_data_func,
+    train_matrix,
+    train_labels_matrix,
+    validation_matrix,
+    validation_labels_matrix,
+    test_matrix,
+    test_labels_matrix,
+    epochs_num=700,
+):
+    train_matrix_hyp = preprocess_data_func(train_matrix)
+    validation_matrix_hyp = preprocess_data_func(validation_matrix)
+    test_matrix_hyp = preprocess_data_func(test_matrix)
+    base_func_hyp = OrdinaryBaseFunc(
+        train_matrix_hyp.shape[1], train_matrix_hyp.shape[0]
+    )
+    loss_func_hyp = MSE(train_matrix_hyp.shape[0])
+    model = Model(base_func_hyp, loss_func_hyp, 0.01)
+
+    train_and_test(
+        model,
+        train_matrix_hyp,
+        train_labels_matrix,
+        validation_matrix_hyp,
+        validation_labels_matrix,
+        epochs_num,
+    )
+
+
+# %%
+def preprocess_data_matrix_1(data_matrix):
+    answer = data_matrix[:, [3]]
+    return answer
+
+
+test_hypothesis(
+    preprocess_data_matrix_1,
+    train_matrix,
+    train_labels_matrix,
+    validation_matrix,
+    validation_labels_matrix,
+    test_matrix,
+    test_labels_matrix,
+)
+
+# %% [markdown]
+# # Second Hypothesis
+
+
+# %%
+def preprocess_data_matrix_2(data_matrix):
+    answer = data_matrix[:, [3, 4]]
+    return answer
+
+
+test_hypothesis(
+    preprocess_data_matrix_2,
+    train_matrix,
+    train_labels_matrix,
+    validation_matrix,
+    validation_labels_matrix,
+    test_matrix,
+    test_labels_matrix,
+)
+
+
+# %% [markdown]
+# # Third Hypothesis
+
+
+# %%
+def preprocess_data_matrix_3(data_matrix):
+    answer = np.column_stack(
+        (data_matrix[:, 3], data_matrix[:, 4], data_matrix[:, 3] * data_matrix[:, 4])
+    )
+    # print(answer.shape)
+    return answer
+
+
+test_hypothesis(
+    preprocess_data_matrix_3,
+    train_matrix,
+    train_labels_matrix,
+    validation_matrix,
+    validation_labels_matrix,
+    test_matrix,
+    test_labels_matrix,
+    1000,
 )
